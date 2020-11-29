a7435553b2709dc866fe38998e4cd362ff894398
trevorsmithdev <trevor.smith.dev@gmail.com>
2020-11-28 22:56:07 -0600

    add login page - WIP

---
+++ pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex">
+        <div class="flex-1"></div>
+        <div class="flex-initial bg-green-200 p-10">
+            <label>Username</label>
+            <input class="block" type="text" />
+            <label>Password</label>
+            <input class="block" type="text" />
+        </div>
+        <div class="flex-1"></div>
+    </div>
+</template>
