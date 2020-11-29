648b16ba5ea80b97170d76951c32a61f93d1624b
trevorsmithdev <trevor.smith.dev@gmail.com>
2020-11-29 15:46:24 -0600

    input component styling

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="flex">
-        <div class="flex-1"></div>
-        <div class="flex-initial bg-green-200 p-10">
+    <div class="pt-10">
+        <div class="p-10 max-w-sm m-auto shadow-md bg-white">
+            <div class="text-2xl text-center mb-10">Login</div>
             <label>Username</label>
-            <input class="block" type="text" />
+            <input class="block mb-2 w-full" type="text" />
             <label>Password</label>
-            <input class="block" type="text" />
+            <input class="block border-black w-full" type="password" />
         </div>
-        <div class="flex-1"></div>
     </div>
 </template>
+
+<style>
+    input {
+        border: 1px solid black;
+    }
+</style>
