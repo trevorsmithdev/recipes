97e7072268288e24f97c0adbd1d4a981f8603752
trevorsmithdev <trevor.smith.dev@gmail.com>
2020-12-09 20:35:01 -0600

    fix proxy config

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/proxy'
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
