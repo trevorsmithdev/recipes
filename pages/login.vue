506c1d2fbbf1ccc018e6807442bba0123b170973
trevorsmithdev <trevor.smith.dev@gmail.com>
2020-12-09 19:49:47 -0600

    button-styling, add try catch around login attempt

--- pages/login.vue
+++ pages/login.vue
@@ -6,7 +6,7 @@
             <input v-model="username" class="block mb-2 w-full" type="text" />
             <label>Password</label>
             <input v-model="password" class="block mb-5 w-full" type="password" />
-            <button @click="login" class="button--green w-full">LOG IN</button>
+            <button @click="login" class="w-full">LOG IN</button>
         </div>
     </div>
 </template>
@@ -17,21 +17,40 @@
         data() {
             return {
                 password: '',
-                username: ''
+                username: '',
+                hasError: true
             }
         },
         methods: {
             async login() {
                 const { username, password } = this
-                const response = await this.$axios.post('/api/login', { username, password })
-                console.log(response)
+                try {
+                    const response = await this.$axios.post('/api/login', { username, password })
+                } catch (err) {
+                    console.error(err)
+                }
             }
         }
     }
 </script>
 
-<style>
+<style lang="scss">
     input {
         border: 1px solid black;
+    }
+
+    button {
+        display: inline-block;
+        border-radius: 4px;
+        border: 1px solid #3b8070;
+        color: #3b8070;
+        text-decoration: none;
+        padding: 10px 30px;
+        background-color: #3b8070;
+        color: #ffffff;
+
+        &:hover {
+            background-color: #296661;
+        }
     }
 </style>
